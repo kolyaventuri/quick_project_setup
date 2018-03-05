62928a2f65c0cf31c0703ee254a39ba78945b9d5
Kolya Venturi <kolya.venturi@enx3s.com>
2018-03-05 16:20:43 -0700

    Initial install script

--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
 { # ensure entire script downloaded
-  INSTALL_DIR = "$HOME/.quick_setup"
-  WORKING_DIR = "$(pwd)"
+  INSTALL_DIR="$HOME/.quick_setup"
+  echo Installing to $INSTALL_DIR
+  WORKING_DIR="$(pwd)"
   if [ ! -d "$INSTALL_DIR" ]; then
     mkdir $INSTALL_DIR
 
     cd $INSTALL_DIR
 
-    
+    git clone git@github.com/kolyaventuri/quick_project_setup.git
 
     cd $WORKING_DIR
+
+    echo "Done installing!"
   else
     echo "ERR: Quick setup already installed."
     echo "Run rm -r $INSTALL_DIR"
