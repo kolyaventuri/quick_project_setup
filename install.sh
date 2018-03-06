3edc741a106861f35f33ae788c2dac887e038ecb
Kolya Venturi <kolya.venturi@enx3s.com>
2018-03-06 00:07:53 -0700

    Updated readme and made install automatic

--- install.sh
+++ install.sh
@@ -1,24 +1,100 @@
 #!/usr/bin/env bash
 
 { # ensure entire script downloaded
-  INSTALL_DIR="$HOME/.quick_setup"
-  echo Installing to $INSTALL_DIR
-  WORKING_DIR="$(pwd)"
-  if [ ! -d "$INSTALL_DIR" ]; then
-    mkdir $INSTALL_DIR
 
-    cd $INSTALL_DIR
+  detect_profile() {
+    if [ -n "${PROFILE}" ] && [ -f "${PROFILE}" ]; then
+      echo "${PROFILE}"
+      return
+    fi
 
-    git clone git@github.com:kolyaventuri/quick_project_setup.git .
+    local DETECTED_PROFILE
+    DETECTED_PROFILE=''
+    local SHELLTYPE
+    SHELLTYPE="$(basename "/$SHELL")"
 
-    cd $WORKING_DIR
+    if [ "$SHELLTYPE" = "bash" ]; then
+      if [ -f "$HOME/.bashrc" ]; then
+        DETECTED_PROFILE="$HOME/.bashrc"
+      elif [ -f "$HOME/.bash_profile" ]; then
+        DETECTED_PROFILE="$HOME/.bash_profile"
+      fi
+    elif [ "$SHELLTYPE" = "zsh" ]; then
+      DETECTED_PROFILE="$HOME/.zshrc"
+    fi
 
-    echo "Done installing!"
-    echo ""
-    echo "Add the following to your ~/.bashrc or ~/.bash_profile to load on start"
-    echo "PATH=\$PATH:$INSTALL_DIR/"
-  else
-    echo "ERR: Quick setup already installed."
-    echo "Run rm -r $INSTALL_DIR"
-  fi
+    if [ -z "$DETECTED_PROFILE" ]; then
+      for EACH_PROFILE in ".profile" ".bashrc" ".bash_profile" ".zshrc"
+      do
+        if DETECTED_PROFILE="$(try_profile "${HOME}/${EACH_PROFILE}")"; then
+          break
+        fi
+      done
+    fi
+
+    if [ ! -z "$DETECTED_PROFILE" ]; then
+      echo "$DETECTED_PROFILE"
+    fi
+  }
+
+  try_profile() {
+    if [ -z "${1-}" ] || [ ! -f "${1}" ]; then
+      return 1
+    fi
+    echo "${1}"
+  }
+
+  install_quick_setup() {
+
+    INSTALL_DIR="$HOME/.quick_setup"
+    echo Installing to $INSTALL_DIR
+    WORKING_DIR="$(pwd)"
+    PROFILE_STRING="[ -s "\$HOME/.quick_setup/setup" ] && \. "\$HOME/.quick_setup/setup""
+    PROFILE=$(detect_profile)
+
+    if [ -z "${PROFILE-}" ] ; then
+      local TRIED_PROFILE
+      if [ -n "${PROFILE}" ]; then
+        TRIED_PROFILE="${NVM_PROFILE} (as defined in \$PROFILE), "
+      fi
+      echo "Profile not found. Tried ${TRIED_PROFILE-}~/.bashrc, ~/.bash_profile, ~/.zshrc, and ~/.profile."
+      echo "Create one of them and run this script again"
+      echo "OR"
+      echo "Append the following lines to the correct file yourself:"
+      command printf "${PROFILE_STRING}"
+      return
+    fi
+
+    if [ ! -d "$INSTALL_DIR" ]; then
+      mkdir $INSTALL_DIR
+
+      cd $INSTALL_DIR
+
+      git clone git@github.com:kolyaventuri/quick_project_setup.git . > /dev/null 2>&1
+
+      cd $WORKING_DIR
+
+      echo "Done installing!"
+      echo ""
+
+      if [[ $(cat $PROFILE) != *"$PROFILE_STRING"* ]]; then
+        echo "" >> $PROFILE
+        echo "" >> $PROFILE
+        echo "# Load Quick Setup Script" >> $PROFILE
+        echo "" >> $PROFILE
+        echo $PROFILE_STRING >> $PROFILE
+      fi
+
+      echo "Script installed to $PROFILE"
+      echo ""
+      echo "Restart terminal or run the following command to use quick setup now"
+      command printf "${PROFILE_STRING}"
+
+    else
+      echo "ERR: Quick setup already installed."
+      echo "Run rm -r $INSTALL_DIR"
+    fi
+  }
+
+  install_quick_setup
 } # ensure entire script downloaded
